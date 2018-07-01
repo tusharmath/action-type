3b4047a2886c3f9da46c83c74d0813e649e4dc69
Tushar Mathur <tusharmath@gmail.com>
2018-07-01 13:39:50 +0530

    fix(isNil): fix typings with isNil

--- src/nil.ts
+++ src/nil.ts
@@ -2,12 +2,12 @@
  * Created by tushar on 25/06/18
  */
 
-import {action} from './action'
+import {Action, action} from './action'
 import {isAction} from './isAction'
 
 const NIL_TYPE = '@@NIL'
 
 export const Nil = action<any>(NIL_TYPE, {})
 
-export const isNil = (obj: any): obj is typeof Nil =>
+export const isNil = (obj: any): obj is Action<any> =>
   isAction(obj) && obj.type === NIL_TYPE
